ccd114cb1c00a69f6254a35e8182a0a0f495c038
Bobby Wettig <bobby@Bobbys-MacBook-Pro.local>
2020-08-29 17:30:18 -0600

    added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './bin'),
+        filename: 'app.js',
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            m: 'mithril'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /\/node_modules\//,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    }
+}
