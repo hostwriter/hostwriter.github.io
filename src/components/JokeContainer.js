ba28473f883e186a386779b358d69d25c93969be
thatDAMNbobby <bobbywettig@gmail.com>
2020-10-14 07:57:02 -0600

    changing machines

--- src/components/JokeContainer.js
+++ src/components/JokeContainer.js
@@ -1,10 +1,13 @@
 const Jokes = require('../models/Joke')
+const { statusById } = require('../models/Likes')
+const Status = require('../models/Likes')
 const JokeObject = require('./JokeObject')
 
 module.exports = {
 
     oninit(vnode) {
         Jokes.load()
+        Status.load()
     },
 
     view(vnode) {
@@ -12,7 +15,8 @@ module.exports = {
             <div>
                 {Jokes.list.length 
                     ? (Jokes.list.map(joke => {
-                        return (<JokeObject {...joke}/>)
+                        let properties = {...joke, ...Status.statusById(joke.id)}
+                        return (<JokeObject {...properties}/>)
                     }))
                     : <h1>Loading...</h1>
                 }
--- src/models/Likes.js
+++ src/models/Likes.js
@@ -1,38 +1,69 @@
 const Joke = require('./Joke')
 const url = require('../config').url()
 
+const actions = [
+    'like',
+    'dislike',
+    'saved'
+]
+
 let Status = {
 
     like: [],
     dislike: [],
+    saved: [],
 
     load: () => {
-        Status.like = localStorage.getItem('like') || []
-        Status.dislike = localStorage.getItem('dislike') || []
+        actions.forEach(action => {
+            Status[action] = JSON.parse(localStorage.getItem(action)) || []
+        })
     },
 
     add: (id, action) => {
         Status.load()
-        //if (Status[action].indexOf(id) !== -1) {
-        //    return
-        //}
-        //Status[action].push(id)
+        console.log(Status)
+        if (Status[action].indexOf(id) !== -1) {
+           return
+        }
+        Status[action].push(id)
         Status.save()
 
-        return m.request({
-            method: "PUT",
-            url: `${url}/${id}/${action}`,
-        })
-        .then(function(result) {
-            console.log(`${action} response: ${result}`)
-        })
+        if (status !== 'saved') {
+            return m.request({
+                method: "PUT",
+                url: `${url}/${id}/${action}`,
+            })
+            .then(function(result) {
+                console.log(`${action} response: ${result}`)
+            })
+        }
     },
 
     save: () => {
-        localStorage.setItem('like', Status.like)
-        localStorage.setItem('dislike', Status.dislike)
-    }
+        actions.forEach(action => {
+            localStorage.setItem(action, JSON.stringify(Status[action])) || []
+        })
+    },
 
+    isLiked: (id) => {
+        return Status.like.indexOf(id) !== -1
+    },
+
+    isDisliked: (id) => {
+        return Status.dislike.indexOf(id) !== -1
+    },
+
+    isSaved: (id) => {
+        return Status.saved.indexOf(id) !== -1
+    },
+
+    statusById: (id) => {
+        return {
+            liked: Status.isLiked(id),
+            disliked: Status.isDisliked(id),
+            saved: Status.isSaved(id),
+        }
+    }
 }
 
 module.exports = Status
