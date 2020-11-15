533f7d16462fa41624af8b01dcf30779ca94f911
Bobby Wettig <bobby@searchspring.com>
2020-11-14 19:00:29 -0700

    switching macines

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -2,10 +2,10 @@
 module.exports = {
 
     view(vnode) {
-        return (
+        return [
             <div>
                 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
-                    <div class="container-fluid">
+                    <div class="container-md">
                     <a class="navbar-brand" href="#">GPT Comedy</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -23,8 +23,8 @@ module.exports = {
                     </div>
                     </div>
                 </nav>
+                </div>,
                 <div class="pt-5 bg-light">{vnode.children}</div>
-            </div>
-        )
+        ]
     }
 }
