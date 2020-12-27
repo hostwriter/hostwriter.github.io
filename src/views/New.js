260702151053c70447a24ce7eb46fc0d573beb9e
thatDAMNbobby <bobbywettig@gmail.com>
2020-12-26 23:38:35 -0700

    ability to change date

--- src/views/New.js
+++ src/views/New.js
@@ -1,13 +1,28 @@
 var JokeContainer = require('../components/JokeContainer')
 var Joke = require('../models/Joke')
+const DatePicker = require('mithril-datepicker')
+const moment = require('moment')
 
 module.exports = {
 
+    oninit() {
+        this.date = Date()
+    },
+
     view() {
         return (
-            <div class={"container-fluid p-5"}>
-                <h1 class="pb-1 text-primary">New jokes</h1>
-                <JokeContainer />
+            <div>
+                <span id={"date-picker"}>
+                    <DatePicker 
+                        onchange={d => Joke.load(moment(d).format('YYYY-MM-DD'))}
+                    />
+                </span>
+                <div class={"container-fluid p-5"}>
+                    <h1 class="pb-1 text-primary">New jokes</h1>
+                    <JokeContainer 
+                        date={this.date}
+                    />
+                </div>
             </div>
         )
     }
